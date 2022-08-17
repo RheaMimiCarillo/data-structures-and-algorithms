d9ab10ecd3255e33578a1464a787accf14cedd1d
Rhea M. Carillo <105750310+RheaMimiCarillo@users.noreply.github.com>
2022-08-16 22:51:42 -0700

    wrote answer 4, but can't test until I get to a real computer

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -64,6 +64,13 @@ For example, twoToThe([1,2,3]) returns [2,4,8] because 2 ^ 1 = 2, 2 ^ 2 = 4, and
 
 const forLoopTwoToThe = (arr) => {
   // Solution code here...
+  let twodleLoo = [];
+
+  for (let i = 0; i < arr.length; i++)
+  {
+    twodleLoo.push(Math.pow(arr[i], arr[i]));
+  }
+  return twodleLoo;
 };
 
 /* ------------------------------------------------------------------------------------------------
