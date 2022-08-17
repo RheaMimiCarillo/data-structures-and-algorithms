3edcec07625e503579b0dc85c760cfe66f7b00b4
RheaMimiCarillo <rheamimicarillo@gmail.com>
2022-08-17 11:19:58 -0700

    challenge 3 working

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -39,6 +39,9 @@ For example, containsAnd(['panda', 'ran', 'and']) returns ['panda', 'and'].
 
 const containsAnd = (arr) => {
   // Solution code here...
+  let andArray = arr.filter(string => string.includes('and') === true);
+
+  return andArray;
 };
 
 /* ------------------------------------------------------------------------------------------------
