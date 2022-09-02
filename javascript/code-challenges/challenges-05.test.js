1d35393819d00e01944fd39fa9ae0e2d40935c2f
RheaMimiCarillo <rheamimicarillo@gmail.com>
2022-09-02 10:21:58 -0700

    challenge 05, problem 6 passing

--- javascript/code-challenges/challenges-05.test.js
+++ javascript/code-challenges/challenges-05.test.js
@@ -166,6 +166,15 @@ Note: You must use reduce for this challenge. You may not use the built-in .reve
 const reversedString = (str) =>
 {
   // Solution code here...
+
+  // split str into an array of chars
+  let strArr = str.split('');
+
+  let reversedStr = strArr.reduce((acc, val) =>
+  {
+    return val + acc;
+  },'');
+  return reversedStr;
 };
 
 /* ------------------------------------------------------------------------------------------------
