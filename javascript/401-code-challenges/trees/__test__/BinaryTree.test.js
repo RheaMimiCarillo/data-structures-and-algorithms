8146217a32a4db9dbdab2ad4d56358ae10a8aa54
RheaMimiCarillo <rheamimicarillo@gmail.com>
2022-10-10 23:27:48 +0000

    pre-order works with console.log

--- javascript/401-code-challenges/trees/__test__/BinaryTree.test.js
+++ javascript/401-code-challenges/trees/__test__/BinaryTree.test.js
@@ -2,7 +2,7 @@
 
 const BinaryTree = require('../src/BinaryTree.js');
 const BinarySearchTree = require('../src/BinarySearchTree.js');
-const Node = require('../src/Node.js');
+const { Node, Knode } = require('../src/Node.js');
 
 describe('Testing tree data structures', () =>
 {
@@ -16,14 +16,23 @@ describe('Testing tree data structures', () =>
   {
     let testTree = new BinaryTree();
     let root = new Node(10);
-
+    root.left = new Node(1);
+    root.right = new Node(50);
     testTree.root = root;
+
+    console.log(testTree.preOrder());
+
+    expect(testTree.root.data).toEqual(10);
   });
 
 
   test('For a Binary Search Tree, can successfully add a left child and right child properly to a node', () =>
   {
+    let testBinaryTree = new BinarySearchTree();
 
+    let root = new Node(10);
+
+    testBinaryTree.root = root;
   });
 
   test('Can successfully return a collection from a pre-order traversal', () =>
--- javascript/401-code-challenges/trees/src/BinaryTree.js
+++ javascript/401-code-challenges/trees/src/BinaryTree.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // import Node class
-const Node = require('./Node');
+const { Node, Knode } = require('./Node.js');
 
 class BinaryTree
 {
@@ -10,9 +10,28 @@ class BinaryTree
     this.root = null;
   }
 
-  preOrder()
+  preOrder(root = this.root)
   {
+    //let result = 'preorder: ';
 
+    if(root !== null)
+    {
+      console.log(root.data);
+      this.preOrder(root.left);
+      this.preOrder(root.right);
+    }
+    /*
+    if (root.left !== null)
+    {
+      this.preOrder(root.left);
+    }
+
+    if (root.right !== null)
+    {
+      this.preOrder(root.right);
+    }
+    */
+    //return result;
   }
 
   inOrder()
