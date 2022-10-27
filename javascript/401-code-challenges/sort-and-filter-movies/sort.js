0126682be5151778ebbce63107ad8723410bd831
RheaMimiCarillo <rheamimicarillo@gmail.com>
2022-10-27 05:45:43 +0000

    added code challenge 28 directory|added whiteboard photos to readme

---
+++ javascript/401-code-challenges/sort-and-filter-movies/sort.js
@@ -0,0 +1,20 @@
+function sortYear(movies)
+{
+  return [];
+}
+
+function sortTitle(movies)
+{
+  return [];
+}
+
+function inGenre(movies, genre)
+{
+  return [];
+}
+
+module.exports = {
+  sortYear,
+  sortTitle,
+  inGenre,
+};
