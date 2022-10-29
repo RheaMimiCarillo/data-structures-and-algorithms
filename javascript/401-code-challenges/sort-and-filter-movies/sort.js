4d1a6e5c12a0588754e5eeb67e5fbe60b03bfcb1
RheaMimiCarillo <rheamimicarillo@gmail.com>
2022-10-29 00:43:37 +0000

    updated readme with API and tests | added comments on my strategy for each challenge

--- javascript/401-code-challenges/sort-and-filter-movies/sort.js
+++ javascript/401-code-challenges/sort-and-filter-movies/sort.js
@@ -1,15 +1,38 @@
+
+
 function sortYear(movies)
 {
+  /* strategy:
+   * use the built-in sort function to with A and B and
+      return b - a
+      - this will work, because we're just dealing with integers
+   */
   return [];
 }
+
+
 
 function sortTitle(movies)
 {
+  /*
+    strategy:
+    look at array and loop through it
+    whilst looping, look at the movie's title and see if it has `the ` (with a space) at the very beginning
+    if it does, strip it out and make a temporary string
+
+    pass title a and title be into the function that removes the 'the ', if applicable, then pass the into the sort to compare the strings to each other
+  */
   return [];
 }
 
+// accepts an array of `movies`
+// and a movie genre string
 function inGenre(movies, genre)
 {
+  /*
+    strategy:
+  use `filter` (which returns an array), and filter out all of the movies that match the target genre
+  */
   return [];
 }
 
