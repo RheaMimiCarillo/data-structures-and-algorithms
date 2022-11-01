29a69a61ec96d2bfbb35c810ad14ae634380ed98
RheaMimiCarillo <rheamimicarillo@gmail.com>
2022-11-01 06:36:04 +0000

    added test cases | tests currently passing | added TODOs to make method more reliable

---
+++ javascript/401-code-challenges/hashmap-repeated-word/HashTable.js
@@ -0,0 +1,253 @@
+'use strict';
+
+const LinkedList = require('./LinkedList');
+
+class HashTable
+{
+  constructor(size)
+  {
+    this.size = size;
+    this.table = new Array(size);
+  }
+
+  /* visualization of hash table:
+
+  const thisTable = [
+    bucket 0: { { key: value } ->  { key2: value2 } -> null },
+    bucket 1: { { key: value } ->  { key2: value2 } -> null },
+    bucket 2: { { key: value } ->  { key2: value2 } -> null }
+  ];
+  */
+
+  /**
+   * Return a hash value for the provided key.
+   * @param {String} key
+   */
+  hash(key)
+  {
+    let charSum = 0;
+    for (let char in key)
+    {
+      // take sum of character codes in `key
+      charSum += key.charCodeAt(char);
+    }
+    // return the charSum, multiplied by a large prime number (599)
+    // then dividing by the number of buckets in this table
+    // and finally returning the remainder
+    // the idea is that the remainder will never be more than the amount of buckets/size in the table
+
+    // returns the index/bucket that this `key` will be placed in
+    /* Example:
+      the key: 'Cat' will determine which bucket this pair goes in
+      Key = "Cat"
+      Value = "Josie"
+
+      67 + 97 + 116 = 280
+
+      280 * 599 = 69648
+
+      69648 % 1024 = 16
+
+      Key gets placed in index of 16.
+
+    */
+    return (charSum * 599) % this.size;
+  }
+
+  /**
+   * places a value into our Hash Table by hashing our key, and setting our value into that Bucket of the table.
+   * @param {String} key
+   * @param {Any} value
+   * should not store a duplicate key:value pair if already in table (so check)
+   */
+  set(key, value)
+  {
+    // .hash() the provided key to get the index of the bucket to .set() to
+    // if the bucket is null, make it into a LinkedList()
+    // and then make a new Node to add to the end of the LinkedList()
+
+    // get the index of the bucket to add to
+    let indexOfBucket = this.hash(key);
+
+    let item = { [ key ]: value };
+    //console.log('key in set: ', key);
+    // console.log('value in set: ', value);
+    // console.log('item in set: ', item);
+    // console.log('set to bucket: ', indexOfBucket);
+
+    // console.log('typeof [key]', typeof(item.key));
+
+    if (this.table[ indexOfBucket ])
+    {
+      //console.log('found something in bucket (hopefully a LinkedList)');
+      this.table[ indexOfBucket ].add(item);
+    }
+    else
+    {
+      //console.log('no LinkedList found in bucket | creating new LinkedList');
+      this.table[ indexOfBucket ] = new LinkedList();
+      this.table[ indexOfBucket ].add(item);
+      //console.log(`bucket [${indexOfBucket}]: `, this.table[ indexOfBucket ]);
+    }
+  }
+
+  /**
+   * returns the value stored in the bucket associated with the key parameter.
+   * @param {String} key
+   * @return {any}, or {null} if no results
+   */
+  get(key)
+  {
+    // using `get` we know the name of the key we want to 'get'
+    // hash the provided key (and use modulus to find the array-index/bucket containing that key)
+    // then traverse the in that bucket/linked-list for any string that matches the 'key'
+    // what to do if there are multiple things to store with same key?
+    // and then with retrieval? should I return an array of results for duplicate keys?
+
+    let indexOfBucket = this.hash(key);
+    let results = [];
+
+    // if the bucket isn't null, then it's a LinkedList
+    if (this.table[ indexOfBucket ])
+    {
+      // wasn't sure how to get .find working without changing the LinkedList's methods
+      //result = this.table[ indexOfBucket ].find(key);
+
+      // get an array of the values in the bucket
+      let pairs = this.table[ indexOfBucket ].values();
+
+      // for each key:value pair in the bucket
+      pairs.forEach(pair =>
+      {
+        // if the pair's key is the same as the key we're looking for
+        //console.log('pair: ', pair);
+        if (Object.keys(pair)[ 0 ] === key)
+        {
+          results.push(pair);
+        }
+      });
+      //console.log('bucket found, result is: ', result);
+      return results;
+    }
+    return null;
+  }
+
+  /**
+   * Returns a Boolean, indication if the key exists in the table.
+   * @param (String) key
+   * @return {Boolean}
+   * does a key already exist in the hash table
+   */
+  has(key)
+  {
+    let indexOfBucket = this.hash(key);
+    let isFound = false;
+
+    if (this.table[ indexOfBucket ])
+    {
+      // resource: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+
+      // put all of the keys from this bucket into an array and check if that array .includes() the key we're looking for
+      isFound = Object.keys(Object.assign({}, ...this.table[ indexOfBucket ].values())).includes(key);
+      //console.log('was it found? ', isFound);
+    }
+    return isFound;
+  }
+
+  /**
+   * Returns a collection of Keys
+   * @return {Array}
+   * What are all of the keys in the hash table?
+   */
+  keys()
+  {
+    // traverse the entire hashtable and return an array of all `keys` in the table
+    // for buckets in this table
+    // if bucket !null
+    // return bucket.values()
+    // by the end of it, we'll have an array with arrays of keys
+
+    // traverse this.table array
+    // if the current bucket isn't null, try to push any keys in the bucket into an array
+    // return the array
+
+    let keysArr = [];
+
+    this.table.forEach(bucket =>
+    {
+      // if the linkedlist in the bucket has at least one node with a value
+      if (bucket.head.value)
+      {
+        // get all of the keys in the bucket
+        let currentBucketKeys = Object.keys(Object.assign({}, ...bucket.values()));
+        // console.log('unique values in current bucket: ', currentBucketKeys);
+
+        keysArr.push(...currentBucketKeys);
+      }
+    });
+
+    // Set() returns an array with only unique values
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+    return [ ... new Set(keysArr) ];
+  }
+  /**
+   * Returns the first instance of a repeated word from a book
+   * @param (String) book
+   * @return {string}
+   * What are all of the keys in the hash table?
+   */
+  repeatedWord(book)
+  {
+    /* Strategy:
+      - accept a book
+      - split book into an array of words (without spaces)
+      - start array traversal loop
+        - at each iteration, look at current word -> strip most punctuation with regex -> make it lowercase
+      - while looping, call .has() to see if the HashTable already has the current word, if it does, then return that word
+        - otherwise, add the current word to the HashTable
+
+    */
+    /* Stretch Goals:
+
+       - Modify your function to return a count of each of the words in the provided string
+         Strategy: similar to above, except instead of looking for .has() matches, create and increment a count for each word that enters the hashmap, then return the count
+
+       - Modify your function to return a list of the words most frequently used in the provided string
+         Strategy:
+
+
+    */
+
+    // split book into an array of words
+    const bookWords = book.split(' ');
+
+    // default return value
+    let repeatedWord = 'No repeated words found';
+
+    // loop through array of words
+    for (let i = 0; i < bookWords.length; i++)
+    {
+      let currentWord = bookWords[i];
+
+      // TODO: strip most non-alphabet characters from currentWord with regex
+      // TODO: make currentWord lowercase
+
+
+      // if the hashtable already has the word (as a key)
+      if (this.has(currentWord))
+      {
+        repeatedWord = currentWord;
+        // break out of loop
+        break;
+      }
+      else
+      {
+        // if not a repeated word, .set() currentWord to hashtable
+        this.set(currentWord, i);
+      }
+    }
+    return repeatedWord;
+  }
+}
+
+module.exports = HashTable;
---
+++ javascript/401-code-challenges/hashmap-repeated-word/__tests__/repeatedWord.test.js
@@ -0,0 +1,39 @@
+'use strict';
+const HashTable = require('../HashTable');
+
+describe('Testing repeatedWord() method', () =>
+{
+  test('Should return null for empty book', () =>
+  {
+    let testHashTable = new HashTable(1024);
+
+    const noBook = '';
+
+    // 'No repeated words found'
+    let result = testHashTable.repeatedWord(noBook);
+
+    expect(result).toEqual('No repeated words found');
+  });
+  test('Should return "No repeated words found" for book without words', () =>
+  {
+    let testHashTable = new HashTable(1024);
+
+    const uniqueBook = 'It was a queer, sultry summer';
+
+    // 'No repeated words found'
+    let result = testHashTable.repeatedWord(uniqueBook);
+
+    expect(result).toEqual('No repeated words found');
+  });
+  test('Should return first instance of a repeated word', () =>
+  {
+    let testHashTable = new HashTable(1024);
+
+    const book = 'Before there was time, before there was anything, there was nothing, and before there was nothing... there were monsters.';
+
+    // 'there'
+    let result = testHashTable.repeatedWord(book);
+
+    expect(result).toEqual('there');
+  });
+});
