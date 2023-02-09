f53cc7b69ef47a8730b05679b58453761b1e7f31
RheaMimiCarillo <rheamimicarillo@gmail.com>
2023-02-09 07:55:24 +0000

    updated solution for challenge 3

--- javascript/quick-code-challenges/filter/src/filter.js
+++ javascript/quick-code-challenges/filter/src/filter.js
@@ -35,10 +35,7 @@ For example, containsAnd(['panda', 'ran', 'and']) returns ['panda', 'and'].
 
 const containsAnd = (arr) =>
 {
-  // Solution code here...
-  let andArray = arr.filter(string => string.includes('and') === true);
-
-  return andArray;
+  return arr.filter(string => string.includes('and') === true);
 };
 
 /* ------------------------------------------------------------------------------------------------
